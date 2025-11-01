13cc3dc2ec6c1b215323b4f891214201105ba57d
christopheralphonse <christopheralphonse96@gmail.com>
2025-11-01 18:47:17 -0400

    lambda

---
+++ packages/cognito/src/lambda/user.ts
@@ -0,0 +1,36 @@
+import type { APIGatewayProxyHandler } from "aws-lambda";
+
+const GH_API_URL= "https://api.github.com/user"
+
+export const handler : APIGatewayProxyHandler = async (event)=>{
+
+ const authHeaders = event.headers['Authorization'];
+
+ if(!authHeaders){
+    return {statusCode:401, body: JSON.stringify({message:"Unauthorized"})}
+ }
+ try {
+    const response = await fetch( GH_API_URL, {
+        method: 'GET',
+        headers:{
+            authorization: "token " + authHeaders.split("Bearer ")[1],
+            accept: "application/json"
+        }
+    })
+    const token = (await response.json() as {id: string})
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            sub: token.id,
+            ...token
+        })
+    }
+ } catch (error) {
+return {
+    statusCode: 500,
+    body: JSON.stringify({ error: error }),
+}
+ }
+
+
+}
