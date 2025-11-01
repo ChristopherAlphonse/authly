5052d9cda75b930e8f493e2ab7de031da70a0c78
christopheralphonse <christopheralphonse96@gmail.com>
2025-11-01 19:01:07 -0400

    .

--- packages/cognito/src/lambda/private.ts
+++ packages/cognito/src/lambda/private.ts
@@ -1,17 +1,18 @@
 import type { APIGatewayProxyWithCognitoAuthorizerHandler } from "aws-lambda";
 
-
 const AllowedOrigins = [
-    process.env.FRONT_END_URL as string|| "http://localhost:5173"
+	(process.env.FRONT_END_URL as string) || "http://localhost:5173",
 ];
-export const handler : APIGatewayProxyWithCognitoAuthorizerHandler = async (event)=>{
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Cognito Authorizer" }),
-        headers:{
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": AllowedOrigins[0],
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        }
-    }
-}
+export const handler: APIGatewayProxyWithCognitoAuthorizerHandler = async (
+	event,
+) => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: "Hello from Cognito Authorizer" }),
+		headers: {
+			"Access-Control-Allow-Headers": "Content-Type",
+			"Access-Control-Allow-Origin": AllowedOrigins[0],
+			"Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+		},
+	};
+};
