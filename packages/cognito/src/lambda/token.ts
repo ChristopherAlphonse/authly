43da47d3109515ac1bc3343d9d71d61e12ed28ad
christopheralphonse <christopheralphonse96@gmail.com>
2025-11-01 19:01:07 -0400

    .

--- packages/cognito/src/lambda/token.ts
+++ packages/cognito/src/lambda/token.ts
@@ -1,28 +1,28 @@
 import type { APIGatewayProxyHandler } from "aws-lambda";
 import parser from "lambda-multipart-parser";
 
-
-const GH_ROOT_PATH = "https://github.com/login/oauth/access_token"
+const GH_ROOT_PATH = "https://github.com/login/oauth/access_token";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    const result = await parser.parse(event);
-    const url = `${GH_ROOT_PATH}?client_id=${result.client_id}&client_secret=${result.client_secret}&code=${result.code}`
+	const result = await parser.parse(event);
+	const url = `${GH_ROOT_PATH}?client_id=${result.client_id}&client_secret=${result.client_secret}&code=${result.code}`;
 
-try {
-    const response = await fetch (url,{method: 'POST', headers: { 'Accept': 'application/json' }})
+	try {
+		const response = await fetch(url, {
+			method: "POST",
+			headers: { Accept: "application/json" },
+		});
 
-    const token =  await response.json();
+		const token = await response.json();
 
-return {
-    statusCode: 200,
-    body: JSON.stringify({ token }),
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ token }),
+		};
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: error }),
+		};
+	}
 };
-
-} catch (error) {
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ error: error }),
-    };
-}
-
-}
